1d41c54ee10dee204c641780b52d1c269f2d32ae
Tristan Seifert <tristan@squee.co>
2015-02-28 18:50:57 -0600

    more ember-cli stuff

---
+++ Brocfile.js
@@ -0,0 +1,40 @@
+/* global require, module */
+
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+var app = new EmberApp();
+
+// Use `app.import` to add additional libraries to the generated
+// output files.
+//
+// If you need to use different assets in different
+// environments, specify an object as the first parameter. That
+// object's keys should be the environment name and the values
+// should be the asset to use in that environment.
+//
+// If the library that you are including contains AMD or ES6
+// modules that you would like to import into your application
+// please specify an object with the list of modules as keys
+// along with the exports of each module as its value.
+
+// MaterialiseCSS
+//app.import('bower_components/materialize/dist/css/materialize.css');
+app.import('bower_components/materialize/dist/js/materialize.js');
+
+app.import("bower_components/materialize/dist/font/material-design-icons/Material-Design-Icons.woff", {
+	destDir: "font/material-design-icons"
+});
+
+app.import("bower_components/materialize/dist/font/roboto/Roboto-Bold.ttf", {
+	destDir: "font/roboto"
+});app.import("bower_components/materialize/dist/font/roboto/Roboto-Light.ttf", {
+	destDir: "font/roboto"
+});app.import("bower_components/materialize/dist/font/roboto/Roboto-Medium.ttf", {
+	destDir: "font/roboto"
+});app.import("bower_components/materialize/dist/font/roboto/Roboto-Regular.ttf", {
+	destDir: "font/roboto"
+});app.import("bower_components/materialize/dist/font/roboto/Roboto-Thin.ttf", {
+	destDir: "font/roboto"
+});
+
+module.exports = app.toTree();
