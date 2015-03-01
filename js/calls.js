9174eaaacea0bd4937c4d7a0171655ab24fcaef0
Austin Reuland <amreuland@gmail.com>
2015-03-01 04:25:47 -0600

    moar

--- js/calls.js
+++ js/calls.js
@@ -4,22 +4,25 @@ SABRE_API_VERION = "v1/";
 
 CONFIG_API_AUTH = "Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTD!ICESMSLB\/CRT.LB!-0123456789012345678!123456!0!ABCDEFGHIJKLM!E2E-1";
 
-SABRE_API_DESTINATION_FINDER_BASE = "shop/flights/fares/";
-SABRE_API_TRAVEL_THEME_LOOKUP_BASE = "lists/supported/shop/themes/";
-SABRE_API_TRAVEL_AIRPORT_LOOKUP_BASE = "lists/supported/shop/themes/";
-SABRE_API_AIRPORT_AT_CITY_LOOKUP_BASE = "list/supported/cities/";
+SABRE_API_DESTINATION_FINDER_BASE       = "shop/flights/fares/";
+SABRE_API_TRAVEL_THEME_LOOKUP_BASE      = "lists/supported/shop/themes/";
+SABRE_API_TRAVEL_AIRPORT_LOOKUP_BASE    = "lists/supported/shop/themes/";
+SABRE_API_AIRPORT_AT_CITY_LOOKUP_BASE   = "lists/supported/cities/";
 
 function makeAPICall(base, args, callback){
-    var resp = "";
+    var arrs = "";
     if(args.length > 0){
-        resp = args[0][0] + "=" + args[0][1];
-        for (var i = 1; i < args.length ; i++) {
-            resp += ("&" + args[i][0] + "=" + args[i][1]);
-        }
+        var resp = [];
+        $.map(args, function(obj, index){
+            if(obj[1] != null){
+                resp.push(obj[0] + "=" + obj[1]);
+            }
+        });
+        arrs = resp.join("&");
     }
     $.ajax(SABRE_API_BASE_URL + SABRE_API_VERION + base, {
         headers: { 'Authorization': CONFIG_API_AUTH },
-        data: resp,
+        data: arrs,
         error: function(jqXHR, textStatus, errorThrown){ callback(null); },
         success: function(data, dataType, dataFilter){ callback(data); }
     });
@@ -33,10 +36,21 @@ function apiAirportLookupByTheme(theme, callback){
     makeAPICall(SABRE_API_TRAVEL_AIRPORT_LOOKUP_BASE + theme, [], callback);
 }
 
-function apiLookupDestination(origin, theme, earliestDepartureDate, maxFare, region, topDestinations){
-    
+function apiLookupDestination(origin, theme, lengthOfStay,
+                              earliestDepartureDate, maxFare,
+                              region, topDestinations, callback){
+    args = [
+        ["origin",origin],
+        ["theme",theme],
+        ["lengthofstay",lengthOfStay],
+        ["earliestdeparturedate",earliestDepartureDate],
+        ["maxFare",maxFare],
+        ["region",region],
+        ["topdestinations",topDestinations]
+    ];
+    makeAPICall(SABRE_API_DESTINATION_FINDER_BASE, args, callback);
 }
 
 function apiGetAirportsInCity(cityCode, callback){
-    makeAPICall(cityCode + "/airports/", [], callback);
+    makeAPICall(SABRE_API_AIRPORT_AT_CITY_LOOKUP_BASE + cityCode + "/airports/", [], callback);
 }
